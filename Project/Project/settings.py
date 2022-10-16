a4b3c8486dbf95a18f713cdea5d2dc58c192ffd6
Gadzet <saygitovg@gmail.com>
2022-10-16 15:13:02 +0300

    Initial

---
+++ Project/Project/settings.py
@@ -0,0 +1,136 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+# Достаем переменные из .env файла
+load_dotenv("example.env")
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# SECRET KEY
+env_secret_key = os.environ.get('PROJECT_SECRET_KEY')
+
+assert env_secret_key is not None, \
+       'Переменная окружения PROJECT_SECRET_KEY не найдена'
+assert env_secret_key != "", \
+       'Неверное значение переменной окружения PROJECT_SECRET_KEY'
+
+SECRET_KEY = env_secret_key
+
+# DEBUG
+evn_debug = os.environ.get('PROJECT_DEBUG')
+
+assert evn_debug is not None, \
+       'Переменная окружения PROJECT_DEBUG не найдена'
+assert evn_debug.lower() in ['true', 'false'], \
+       'Неверное значение переменной окружения PROJECT_DEBUG'
+
+DEBUG = evn_debug.lower() == 'true'
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'homepage.apps.HomepageConfig',
+    'catalog.apps.CatalogConfig',
+    'about.apps.AboutConfig',
+
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'Project.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'Project.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.'
+                'NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ README.md
@@ -0,0 +1,9 @@
+<h1>Домашка</h1>
+<h3>Инструкция по установке:</h3>
+<ul>
+  <li>Загрузите проект: git clone https://github.com/Gadzet005/Homework</li>
+  <li>Создайте виртуальное окружение: python -m venv venv</li>
+  <li>Зайдите в него: venv/Scripts/Activate.ps1 (Зависит от ОС, эта команда работает для винды)</li>
+  <li>Загрузите внешние зависимости: pip install -r requirements.txt</li>
+  <li>Запустите проект: python Project/manage.py runserver</li>
+</ul>
