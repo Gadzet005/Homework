9f0a4af4fabf53b63562413c68254c51d6eab95e
Gadzet <saygitovg@gmail.com>
2022-10-25 19:04:59 +0300

    Edit subtest

--- Project/catalog/tests.py
+++ Project/catalog/tests.py
@@ -9,19 +9,14 @@ class URLTests(TestCase):
             )
 
     def test_item_detail(self):
-        def test_item_pk(pk, expected_code):
-            response = Client().get(f'/catalog/{pk}/')
-            with self.subTest(
-                    'Элемент каталога вернул неправильный статус', pk=pk
-                    ):
-                self.assertEqual(response.status_code, expected_code)
-
         # Тестовые случаи
         pk_tests = {
-            200: (1, 12, 100),
+            200: (1, 12, 100, -1),
             404: (0, -1, 1.1, '000111', 'Hello', '1 1')
         }
 
         for expected_code, pk_list in pk_tests.items():
             for pk in pk_list:
-                test_item_pk(pk, expected_code)
+                with self.subTest('Тест сatalog pk', pk=pk):
+                    response = Client().get(f'/catalog/{pk}/')
+                    self.assertEqual(response.status_code, expected_code)
