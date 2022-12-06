38bec8578390ff2bedba365bf20f31aa4c263d20
Denis_Burkov <dburkov05@bk.ru>
2022-12-07 01:59:08 +0500

    добавлен тест для контекст-процессора

--- Project/Users/tests.py
+++ Project/Users/tests.py
@@ -1,3 +1,24 @@
-# from django.test import TestCase
+import datetime
 
-# ToDo: стоит добавить тесты
+from django.test import TestCase, Client
+
+from Users.models import User
+
+
+class ContextProcessorTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+    def test_exist(self):
+        now = datetime.date.today()
+        users = User.objects.filter(
+            birthday_date__month=now.month,
+            birthday_date__day=now.day,
+        )
+        response = Client().get('/')
+        self.assertEqual(
+            response.status_code, 200, 'Главная страница не работает'
+            )
+        self.assertIn('users_with_birthday', response.context)
+        self.assertEqual(len(response.context['users_with_birthday']), len(users))
